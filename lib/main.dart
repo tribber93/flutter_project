d17a60a35b291b59f7022e57a1fb3bd7b6893247
tribber93 <tribberyoni5@gmail.com>
2022-03-26 14:44:10 +0700

    pertama

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 64, 35, 196),
+          title: const Text('Hello Guys Yoni Tribber Disini'),
+        ),
+        body: const Center(
+          child: Text('Hello Ini adalah my first app'),
+        ),
+      ),
+    );
+  }
+}
