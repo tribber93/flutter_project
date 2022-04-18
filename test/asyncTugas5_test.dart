d27400856c59cf568a9b4b19fa023cf536e053d4
tribber93 <tribberyoni5@gmail.com>
2022-04-18 22:07:48 +0700

    Tugas 5 Asynchronous

---
+++ test/asyncTugas5_test.dart
@@ -0,0 +1,28 @@
+void main() async {
+  print('Mencari film...');
+  try {
+    var pencarian = cariFilm('Doctor Strange');
+    print(await pencarian);
+  } catch (e) {
+    print(e);
+  } finally {
+    print('Selesai');
+  }
+}
+
+listFilm() {
+  var film = ['Avengers', 'Spider-Man', 'Batman', 'Iron Man', 'Doctor Strange'];
+  return film;
+}
+
+Future cariFilm(judulFilm) {
+  return Future.delayed(Duration(seconds: 4), () {
+    List<String> film = listFilm();
+    for (int i = 0; i < film.length; i++) {
+      if (film[i] == judulFilm) {
+        return 'Yeay! Film $judulFilm ditemukan';
+      }
+    }
+    throw Exception('Film $judulFilm tidak ditemukan');
+  });
+}
